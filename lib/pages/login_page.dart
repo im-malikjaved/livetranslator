1ba1a93d1289c6bcc689491bd7a077d93b2d7700
im-malikjaved <muhammadjaved444@gmail.com>
2022-03-26 18:50:47 +0500

    Change the name

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -35,7 +35,7 @@ class _LoginPageState extends State<LoginPage> {
                 children: [
                   TextFormField(
                     decoration: const InputDecoration(
-                      hintText: "Enter username",
+                      hintText: "Please Enter username",
                       labelText: "username",
                     ),
                     onChanged: (value) {
