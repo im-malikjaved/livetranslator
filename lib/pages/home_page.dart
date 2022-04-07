e28c5594cfee27eb98db17697a924ba155647ade
im-malikjaved <muhammadjaved444@gmail.com>
2022-04-07 23:32:26 +0500

    working ok

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,7 +16,7 @@ class HomePage extends StatelessWidget {
       body: Center(
         // ignore: avoid_unnecessary_containers
         child: Container(
-          child: Text("Welcome to enemy $days of flutter by $name"),
+          child: Text("Welcome to enemy is $days of flutter by $name"),
         ),
       ),
       drawer: const MyDrawer(),
